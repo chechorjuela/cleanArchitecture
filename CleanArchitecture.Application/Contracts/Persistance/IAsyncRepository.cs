2d8144376422851453579290ca092a532cec672f
Sergiorjuela <saor89@hotmail.com>
2024-03-06 10:08:24 -0500

    first commit~

---
+++ CleanArchitecture.Application/Contracts/Persistance/IAsyncRepository.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Common;
+
+
+namespace CleanArchitecture.Application.Contracts.Persistance
+{
+    public interface IAsyncRepository<T> where T : BaseDomainModel
+    {
+    }
+}
---
+++ CleanArchitecture.Domain/Common/BaseDomainModel.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArchitecture.Domain.Common
+{
+    public abstract class BaseDomainModel
+    {
+        public int Id { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public string? CreatedBy { get; set; }
+        public DateTime? UpdateAt { get; set;}
+        public string? UpdatedBy { get; set;}
+    }
+}
